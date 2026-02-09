6e99713d49b98f92d7171815dbd451cd9380229f
raffaelesmakaj66-design <raffaelesmakaj66@gmail.com>
2026-02-09 16:59:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import TelegramBot from "node-telegram-bot-api";
-import sqlite3 from "sqlite3";
+const TelegramBot = require("node-telegram-bot-api");
+const sqlite3 = require("sqlite3").verbose();
 sqlite3.verbose();
 
 // =====================
